b0813706f45c7c735e143d7a76c03a191b1cf4a7
Andrew <andrewjuang01@gmail.com>
2021-05-09 20:47:22 -0400

    Fixes

--- src/TopoTraversal/gui/__init__.py
+++ src/TopoTraversal/gui/__init__.py
@@ -1,7 +1,12 @@
-import Tkinter as tk
+import tkinter as tk
 
 def start():
-    #stuff
+    window=tk.Tk()
+    btn=tk.Button(window, text="This is Button widget", fg='blue')
+    btn.place(x=80, y=100)
+    window.title('Hello Python')
+    window.geometry("300x200+10+10")
+    window.mainloop()
 
-def update():
+#def update():
     #more stuff
